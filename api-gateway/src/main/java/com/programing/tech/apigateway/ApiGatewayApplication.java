27568c695a535b0c7e25df72c17727b3f5292128
romeotarko <romeo.tarko@outlook.com>
2023-09-04 13:24:22 +0200

    Minor changes on package name

--- api-gateway/src/main/java/com/programing/tech/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/programing/tech/apigateway/ApiGatewayApplication.java
@@ -1,4 +1,4 @@
-package programing.texh.apigateway;
+package com.programing.tech.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
